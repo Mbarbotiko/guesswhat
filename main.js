22ffac82f84182fafd3cb213a6dc0e04d9497db6
Margarita Barbotiko <mbarbotiko@gmail.com>
2018-10-06 11:26:28 -0500

    adding conditional statements for win lose

--- main.js
+++ main.js
@@ -7,35 +7,51 @@ let losses = 0;
 
 function gameReset() {
     guessedLetters = [];
-    cpuRandomLetter();
     guessesLeft = 10;
-    wins = 0;
-    losses = 0;
-
-
+    cpuRandomLetter();
 }
+
 function cpuRandomLetter() {
-    cpuLetterToGuess = cpuLetters[Math.floor(Math.random() * cpuLetters.length)]
+    cpuLetterToGuess = cpuLetters[Math.floor(Math.random() * cpuLetters.length)] // can just move this into the game function and now need to reset it in gamereset
 }
+
+cpuRandomLetter();
 //using math random to assign a random letter from the cpuLetters array to the computers guess
 
+document.onkeyup = function (event) {
+    var userChoice = String.fromCharCode(event.which).toLowerCase();
+    guessedLetters.push(userChoice);
 
 
 
 
+    console.log(cpuLetterToGuess);
+    //write a condition / evaluation of the users choice if they choose anything besides what is in the CPU letters array tell them to choose a letter
 
 
 
 
+    if (userChoice===cpuLetterToGuess){
+        wins++;
+        gameReset();
+    }
+    
+    if (guessesLeft === 0){
+        losses++;
+        gameReset();
+    }
+
+    guessesLeft--
+
+    document.getElementById('wins').innerHTML = '<h2>' + wins + '</h2>'
+    document.getElementById('losses').innerHTML = '<h2>' + losses + '</h2>'
+    document.getElementById('guesses-left').innerHTML = '<h2>' + guessesLeft + '</h2>'
+    document.getElementById('guesses-made').innerHTML = '<h2>' + guessedLetters + '</h2>'
+    //will need to use a loop to print all the letters
+    
+};
 
 
 
-
-
-document.getElementById('wins').innerHTML = '<h2>' + wins + '</h2>'
-document.getElementById('losses').innerHTML = '<h2>' + losses + '</h2>'
-document.getElementById('guesses-left').innerHTML = '<h2>' + guessesLeft + '</h2>'
-document.getElementById('guesses-made').innerHTML = '<h2>' + guessedLetters + '</h2>'
-//will need to use a loop to print all the letters
 
 
