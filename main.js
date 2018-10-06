b54168298cdf3b656e5ec5bd8d0e1d119a0c8804
Margarita Barbotiko <mbarbotiko@gmail.com>
2018-10-06 10:52:14 -0500

    adding reset function

--- main.js
+++ main.js
@@ -1,10 +1,23 @@
 const cpuLetters = ['a', 'b'];
 let guessedLetters = [];
 let cpuLetterToGuess = null;
-const guessesLeft = 10;
+let guessesLeft = 10;
 let wins = 0;
 let losses = 0;
 
+function gameReset() {
+    guessedLetters = [];
+    cpuRandomLetter();
+    guessesLeft = 10;
+    wins = 0;
+    losses = 0;
+
+
+}
+function cpuRandomLetter() {
+    cpuLetterToGuess = cpuLetters[Math.floor(Math.random() * cpuLetters.length)]
+}
+//using math random to assign a random letter from the cpuLetters array to the computers guess
 
 
 
@@ -17,12 +30,12 @@ let losses = 0;
 
 
 
-document.getElementById('wins').innerHTML = '<h2>' + wins +  '</h2>'
 
 
-document.getElementById('losses').innerHTML='<h2>'+losses+'</h2>'
-document.getElementById('guesses-left').innerHTML='<h2>'+guessesLeft+'</h2>'
-document.getElementById('guesses-made').innerHTML='<h2>'+guessedLetters+'</h2>'
+document.getElementById('wins').innerHTML = '<h2>' + wins + '</h2>'
+document.getElementById('losses').innerHTML = '<h2>' + losses + '</h2>'
+document.getElementById('guesses-left').innerHTML = '<h2>' + guessesLeft + '</h2>'
+document.getElementById('guesses-made').innerHTML = '<h2>' + guessedLetters + '</h2>'
 //will need to use a loop to print all the letters
 
 
